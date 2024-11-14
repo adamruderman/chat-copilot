48c54ec383ba14d659d816b4fc6af692ef35e7bf
Aaron Barth <aaronba@users.noreply.github.com>
2024-11-13 19:27:06 -0500

    Update deploy-azure.sh
    Adding in Bill's deploy azure script.

--- scripts/deploy/deploy-azure.sh
+++ scripts/deploy/deploy-azure.sh
@@ -5,7 +5,7 @@
 set -e
 
 usage() {
-    echo "Usage: $0 -d DEPLOYMENT_NAME -s SUBSCRIPTION -c BACKEND_CLIENT_ID -fc FRONTEND_CLIENT_ID -t AZURE_AD_TENANT_ID -ai AI_SERVICE_TYPE [OPTIONS]"
+    echo "Usage: $0 -d DEPLOYMENT_NAME -s SUBSCRIPTION -c BACKEND_CLIENT_ID -fc FRONTEND_CLIENT_ID -t AZURE_AD_TENANT_ID -ai AI_SERVICE_TYPE -cm COMPLETION_MODEL -em EMBEDDING_MODEL [OPTIONS]"
     echo ""
     echo "Arguments:"
     echo "  -d, --deployment-name DEPLOYMENT_NAME      Name for the deployment (mandatory)"
@@ -14,6 +14,8 @@ usage() {
     echo "  -fc, --frontend-client-id FE_CLIENT_ID     Azure AD client ID for the frontend app registration (mandatory)"
     echo "  -t, --tenant-id AZURE_AD_TENANT_ID         Azure AD tenant ID for authenticating users (mandatory)"
     echo "  -ai, --ai-service AI_SERVICE_TYPE          Type of AI service to use (i.e., OpenAI or AzureOpenAI) (mandatory)"
+    echo "  -cm, --completion-model COMPLETION_MODEL   Model to use for chat completions (mandatory)"
+    echo "  -em, --embedding-model EMBEDDING_MODEL     Model to use for text embeddings (mandatory)"
     echo "  -aiend, --ai-endpoint AI_ENDPOINT          Endpoint for existing Azure OpenAI resource"
     echo "  -aikey, --ai-service-key AI_SERVICE_KEY    API key for existing Azure OpenAI resource or OpenAI account"
     echo "  -rg, --resource-group RESOURCE_GROUP       Resource group to which to make the deployment (default: \"rg-\$DEPLOYMENT_NAME\")"
@@ -28,6 +30,8 @@ usage() {
     echo "  -ws, --deploy-web-searcher-plugin          Deploy the web searcher plugin"
     echo "  -dd, --debug-deployment                    Switches on verbose template deployment output"
     echo "  -ndp, --no-deploy-package                  Skips deploying binary packages to cloud when set."
+    echo "  -aoaigr, --aoai-resource-group-name        Azure OpenAI service resource group name"
+    echo "  -aoaian, --aoai-account-name               Azure OpenAI resource account name"
 }
 
 # Parse arguments
@@ -64,6 +68,16 @@ while [[ $# -gt 0 ]]; do
         shift
         shift
         ;;
+    -cm | --completion-model)
+        COMPLETION_MODEL="$2"
+        shift
+        shift
+        ;;
+    -em | --embedding-model)
+        EMBEDDING_MODEL="$2"
+        shift
+        shift
+        ;;
     -aikey | --ai-service-key)
         AI_SERVICE_KEY="$2"
         shift
@@ -95,7 +109,7 @@ while [[ $# -gt 0 ]]; do
         shift
         ;;
     -ms | --memory-store)
-        MEMORY_STORE=="$2"
+        MEMORY_STORE="$2"
         shift
         ;;
     -nc | --no-cosmos-db)
@@ -118,6 +132,25 @@ while [[ $# -gt 0 ]]; do
         NO_DEPLOY_PACKAGE=true
         shift
         ;;
+    -aoaigr | --aoai-resource-group-name)
+        AOAI_RESOURCE_GROUP_NAME="$2"
+        shift
+        shift
+        ;;
+    -aoaian | --aoai-account-name)
+        AOAI_ACCOUNT_NAME="$2"
+        shift
+        shift
+        ;;
+    -e | --environment)
+        ENVIRONMENT="$2"        
+        if [[ "$ENVIRONMENT" != "AzureCloud" && "$ENVIRONMENT" != "AzureUSGovernment" ]]; then
+            echo "Invalid environment option. Use 'AzureCloud' or 'AzureUSGovernment'."
+            exit 1
+        fi
+        shift
+        shift
+        ;;
     *)
         echo "Unknown option $1"
         usage
@@ -127,7 +160,7 @@ while [[ $# -gt 0 ]]; do
 done
 
 # Check mandatory arguments
-if [[ -z "$DEPLOYMENT_NAME" ]] || [[ -z "$SUBSCRIPTION" ]] || [[ -z "$BACKEND_CLIENT_ID" ]] || [[ -z "$FRONTEND_CLIENT_ID" ]] || [[ -z "$AZURE_AD_TENANT_ID" ]] || [[ -z "$AI_SERVICE_TYPE" ]]; then
+if [[ -z "$DEPLOYMENT_NAME" ]] || [[ -z "$SUBSCRIPTION" ]] || [[ -z "$BACKEND_CLIENT_ID" ]] || [[ -z "$FRONTEND_CLIENT_ID" ]] || [[ -z "$AZURE_AD_TENANT_ID" ]] || [[ -z "$AI_SERVICE_TYPE" ]] || [[ -z "$COMPLETION_MODEL" ]] || [[ -z "$EMBEDDING_MODEL" ]]; then
     usage
     exit 1
 fi
@@ -164,13 +197,29 @@ if [[ "${AI_SERVICE_TYPE,,}" = "openai" ]] && [[ -z "$AI_SERVICE_KEY" ]]; then
     usage
     exit 1
 fi
-
 # If resource group is not set, then set it to rg-DEPLOYMENT_NAME
 if [ -z "$RESOURCE_GROUP" ]; then
     RESOURCE_GROUP="rg-${DEPLOYMENT_NAME}"
 fi
 
 TEMPLATE_FILE="$(dirname "$0")/main.bicep"
+
+# Ensure that the environment variable is set
+if [[ -z "$ENVIRONMENT" ]]; then
+    echo "Error: Environment not set. Please set the environment to either 'AzureCloud' or 'AzureUSGovernment'."
+    exit 1
+fi
+
+echo "Setting Azure cloud environment to $ENVIRONMENT..."
+az cloud set --name "$ENVIRONMENT"
+
+# Check if the last command was successful
+if [[ $? -ne 0 ]]; then
+    echo "Error: Failed to set Azure cloud environment to $ENVIRONMENT."
+    exit $?
+fi
+
+echo "Azure cloud environment successfully set to $ENVIRONMENT."
 
 az account show --output none
 if [ $? -ne 0 ]; then
@@ -182,19 +231,34 @@ az account set -s "$SUBSCRIPTION"
 
 # Set defaults
 : "${REGION:="southcentralus"}"
-: "${WEB_APP_SVC_SKU:="B1"}"
+: "${WEB_APP_SVC_SKU:="I2V2"}"
 : "${AZURE_AD_INSTANCE:="https://login.microsoftonline.com"}"
 : "${MEMORY_STORE:="AzureAISearch"}"
 : "${NO_COSMOS_DB:=false}"
 : "${NO_SPEECH_SERVICES:=false}"
 : "${DEPLOY_WEB_SEARCHER_PLUGIN:=false}"
 
+# Check environment and adjust region and Azure AD instance
+if [[ "$ENVIRONMENT" == "AzureUSGovernment" ]]; then
+    echo "updating values for Government Cloud"
+    REGION="usgovvirginia"  # Direct assignment
+    AZURE_AD_INSTANCE="https://login.microsoftonline.us"  # Direct assignment
+else
+    echo "Unknown environment: $ENVIRONMENT"
+    exit 1
+fi
+
+# Log the values of REGION and AZURE_AD_INSTANCE
+echo "Region set to: $REGION"
+echo "Azure AD Instance set to: $AZURE_AD_INSTANCE"
 # Create JSON config
 JSON_CONFIG=$(
     cat <<EOF
 {
     "webAppServiceSku": { "value": "$WEB_APP_SVC_SKU" },
     "aiService": { "value": "$AI_SERVICE_TYPE" },
+    "completionModel": { "value": "$COMPLETION_MODEL" },
+    "embeddingModel": { "value": "$EMBEDDING_MODEL" },
     "aiApiKey": { "value": "$AI_SERVICE_KEY" },
     "deployPackages": { "value": $([ "$NO_DEPLOY_PACKAGE" = true ] && echo "false" || echo "true") },
     "aiEndpoint": { "value": "$([ ! -z "$AI_ENDPOINT" ] && echo "$AI_ENDPOINT")" },
@@ -202,6 +266,8 @@ JSON_CONFIG=$(
     "azureAdTenantId": { "value": "$AZURE_AD_TENANT_ID" },
     "webApiClientId": { "value": "$BACKEND_CLIENT_ID" },
     "frontendClientId": { "value": "$FRONTEND_CLIENT_ID" },
+    "AOAIResourceGroupName": { "value": "$AOAI_RESOURCE_GROUP_NAME" },
+    "AOAIAccountName": { "value": "$AOAI_ACCOUNT_NAME" },
     "deployNewAzureOpenAI": { "value": $([ "$NO_NEW_AZURE_OPENAI" = true ] && echo "false" || echo "true") },
     "memoryStore": { "value": "$MEMORY_STORE" },
     "deployCosmosDB": { "value": $([ "$NO_COSMOS_DB" = true ] && echo "false" || echo "true") },
@@ -218,6 +284,7 @@ echo "Validating template file..."
 az deployment group validate --name "$DEPLOYMENT_NAME" --resource-group "$RESOURCE_GROUP" --template-file "$TEMPLATE_FILE" --parameters "$JSON_CONFIG"
 
 echo "Deploying Azure resources ($DEPLOYMENT_NAME)..."
+
 if [ "$DEBUG_DEPLOYMENT" = true ]; then
     az deployment group create --name "$DEPLOYMENT_NAME" --resource-group "$RESOURCE_GROUP" --template-file "$TEMPLATE_FILE" --debug --parameters "$JSON_CONFIG"
 else
